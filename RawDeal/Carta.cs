e040d507229718b96f42a0d0bf55f0599d178b5d
Franco Anfossi <franco.anfossi@uc.cl>
2023-08-31 00:54:06 -0400

    cambio en print carta

--- RawDeal/Carta.cs
+++ RawDeal/Carta.cs
@@ -12,11 +12,6 @@ public class Carta
 
     public override string ToString()
     {
-        string text = $"{Title} - ";
-        foreach (var lol in Subtypes)
-        {
-            text += lol + ", ";
-        }
-        return text;
+        return $"{Title}";
     }
 }
